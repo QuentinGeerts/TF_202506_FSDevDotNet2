a61122bb5cc9ca2dd36d256501c16449b46ee418
Quentin Geerts <quentin.geerts@bstorm.be>
2025-06-02 16:11:31 +0200

    Add Stored Procedures

---
+++ TF_202506_FSDevDotNet2.ADO/StoredProcedures/AddTask.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[AddTask]
+	@Title NVARCHAR(50),
+	@Description NVARCHAR(MAX) = NULL,
+	@Status NVARCHAR(20),
+	@UserId INT = NULL
+AS
+	SET NOCOUNT ON;
+
+	IF @UserId IS NOT NULL AND NOT EXISTS (SELECT 1 FROM [dbo].[User] WHERE [Id] = @UserId AND [IsActive] = 1) 
+	BEGIN
+		RAISERROR('User does not exist', 16, 1);
+		RETURN -1;
+	END
+
+	INSERT INTO [dbo].[Task] ([Title], [Description], [Status],[UserId])
+	VALUES (@Title, @Description, @Status, @UserId);
+
+	RETURN SCOPE_IDENTITY();
+
+RETURN 0
---
+++ TF_202506_FSDevDotNet2.ADO/StoredProcedures/AddUser.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[AddUser]
+	@UserName NVARCHAR(50),
+	@Email NVARCHAR(100),
+	@Password NVARCHAR(255),
+	@LastName NVARCHAR(50) = NULL,
+	@FirstName NVARCHAR(50) = NULL
+AS
+	SET NOCOUNT ON;
+
+
+	IF EXISTS (SELECT 1 FROM [dbo].[User] WHERE [UserName] = @UserName)
+	BEGIN
+		RAISERROR('UserName already taken.', 16, 1);
+		RETURN -1;
+	END
+
+	IF EXISTS (SELECT 1 FROM [dbo].[User] WHERE [Email] = @Email)
+	BEGIN
+		RAISERROR('Email already taken.', 16, 1);
+		RETURN -1;
+	END
+
+	INSERT INTO [dbo].[User] (UserName, Email, PasswordHash, LastName, FirstName)
+	VALUES (@UserName, @Email, HASHBYTES('SHA2_256', @Password), @LastName, @FirstName);
+
+	RETURN SCOPE_IDENTITY(); -- Permet de récupérer l'id créé suite à l'insertion
+
+RETURN 0
