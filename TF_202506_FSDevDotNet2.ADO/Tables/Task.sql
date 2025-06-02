9cebe788a63e0c5f4dfb917b71b01c5ead4d3ed7
Quentin Geerts <quentin.geerts@bstorm.be>
2025-06-02 15:21:51 +0200

    Add project files.

---
+++ TF_202506_FSDevDotNet2.ADO/Tables/Task.sql
@@ -0,0 +1,44 @@
+﻿CREATE TABLE [dbo].[Task]
+(
+	[Id] INT NOT NULL IDENTITY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [Status] NVARCHAR(20) NOT NULL, 
+    [CompletedAt] DATETIME2 NULL, 
+    [UserId] INT NULL, 
+    [CreatedAt] DATETIME2 NULL DEFAULT GETDATE(), 
+    [UpdatedAt] DATETIME2 NULL DEFAULT GETDATE(), 
+    [IsActive] BIT NULL DEFAULT 1,
+    CONSTRAINT [PK_Task] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_Task_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
+
+GO
+
+CREATE INDEX [IX_Task_Title] ON [dbo].[Task] ([Title])
+
+GO
+
+CREATE TRIGGER [dbo].[TR_Task_UpdatedAt]
+    ON [dbo].[Task]
+    AFTER UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+
+        UPDATE [dbo].[Task]
+        SET [UpdatedAt] = GETDATE()
+        WHERE [Id] = (SELECT [Id] FROM inserted);
+    END
+GO
+
+CREATE TRIGGER [dbo].[TR_Task_IsActive]
+    ON [dbo].[Task]
+    INSTEAD OF DELETE
+    AS
+    BEGIN
+        SET NoCount ON
+        UPDATE [dbo].[Task]
+        SET [IsActive] = 0
+        WHERE [Id] = (SELECT [Id] FROM deleted);
+    END
---
+++ TF_202506_FSDevDotNet2.ADO/Tables/User.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL IDENTITY, 
+    [UserName] NVARCHAR(50) NOT NULL,
+    [Email] NVARCHAR(100) NOT NULL,
+    [PasswordHash] NVARCHAR(255) NOT NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [CreatedAt] DATETIME2 NULL DEFAULT GETDATE(), 
+    [UpdatedAt] DATETIME2 NULL DEFAULT GETDATE(), 
+    [IsActive] BIT NULL DEFAULT 1, 
+    CONSTRAINT [PK_User] PRIMARY KEY ([Id]), 
+    CONSTRAINT [UK_User_UserName] UNIQUE ([UserName]),
+    CONSTRAINT [UK_User_Email] UNIQUE ([Email]), 
+    CONSTRAINT [CK_User_Email_Format] CHECK ([Email] LIKE '_%@_%.__%'),
+)
+
+GO
+
+CREATE TRIGGER [dbo].[TR_User_UpdatedAt]
+    ON [dbo].[User]
+    AFTER UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+
+        UPDATE [dbo].[User]
+        SET [UpdatedAt] = GETDATE()
+        WHERE [Id] = (SELECT [Id] FROM inserted);
+    END
+GO
+
+CREATE TRIGGER [dbo].[TR_User_IsActive]
+    ON [dbo].[User]
+    INSTEAD OF DELETE
+    AS
+    BEGIN
+        SET NoCount ON
+        UPDATE [dbo].[User]
+        SET [IsActive] = 0
+        WHERE [Id] = (SELECT [Id] FROM deleted);
+    END
